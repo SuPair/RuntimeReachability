03f7285e967c7bed94d94647a992b52b67c5461f
SuAnn <suann@MacBook-Pro.local>
2015-08-06 14:26:37 +0800

    Initial Commit

---
+++ RuntimeReachability/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RuntimeReachability
+//
+//  Created by SuAnn on 15/8/6.
+//  Copyright (c) 2015年 SuAnn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RuntimeReachability/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RuntimeReachability
+//
+//  Created by SuAnn on 15/8/6.
+//  Copyright (c) 2015年 SuAnn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
